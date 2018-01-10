eb7138ea4df7a9c8329baf0a346b0908744c35fb
Alexander Lyulko <alexander-lyulko@surrface.xyz>
2018-01-10 05:02:53 +0200

    jQuery test

---
+++ slider.js
@@ -0,0 +1,54 @@
+$(window).on('load', (() => {
+    const dots = $('.dot');
+    let current = 0;
+    $(dots[current]).css({background: 'white'});
+
+    function changeCaptionBack(slider) {
+        if (slider.classList.contains('current')) {
+            $(slider).find('.slide-caption').slideDown(200).animate({'border-top-right-radius': '0%',}, 200);
+
+        } else {
+            $(slider).find('.slide-caption').slideUp(200).animate({'border-top-right-radius': '40%',}, 200);
+        }
+    }
+
+    $('.slide').each(function () {
+        $(this).css({
+            backgroundImage: `url(${this.dataset.imageurl})`,
+        });
+        changeCaptionBack(this);
+    });
+
+
+    $(dots).each(function () {
+        $(this).click(function () {
+            const id = parseInt(this.dataset.id);
+            if (id !== current) {
+                $(dots).each(function () {
+                    $(this).css({
+                        background: 'transparent',
+                    });
+                });
+                $(this).css({
+                    background: '#fff',
+                });
+
+                const showMe = $(`.slide[data-sliderid=${id}]`)[0];
+                const hideMe = $(`.slide[data-sliderid=${current}]`)[0];
+
+                $(showMe).css({zIndex: 4});
+                $(hideMe)
+                    .animate({top: '30%'}, 300)
+                    .animate({top: '100%', zIndex: 2}, 300, null, () => {
+                        hideMe.classList.remove('current');
+                        showMe.classList.add('current');
+                        $(hideMe).css({top: '0%',});
+                        $(showMe).css({top: '0%', zIndex: 5});
+                        changeCaptionBack(hideMe);
+                        changeCaptionBack(showMe);
+                    });
+                current = id;
+            }
+        });
+    });
+}));
